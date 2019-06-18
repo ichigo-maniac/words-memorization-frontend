827c8ebdd987d87476cb031c55fa09f5da95993d
impi <ichigomaniac@gmail.com>
2019-06-18 19:13:26 +0300

    Init commit

---
+++ src/main/java/com/words/memorization/wordsmemorizationfrontend/WordsMemorizationFrontendApplication.java
@@ -0,0 +1,13 @@
+package com.words.memorization.wordsmemorizationfrontend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WordsMemorizationFrontendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WordsMemorizationFrontendApplication.class, args);
+    }
+
+}
