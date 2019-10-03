b762e8b814b015a233c3a3c4cb7aedee35341e8b
Konoplev Dmitriy <konoplev@yarnord.ru>
2019-10-03 11:42:17 +0300

    Added first view of sidebar

--- src/main/js/sidebar.js
+++ src/main/js/sidebar.js
@@ -2,24 +2,46 @@
 
 import React from "react";
 
+/**
+ * Sidebar component's class
+ */
 class Sidebar extends React.Component{
+
+    constructor(props) {
+        super(props);
+        this.state = {activeLink : null};
+        /** Bind listeners */
+        this.handleItemClick = this.handleItemClick.bind(this);
+    }
+
+    handleItemClick(linkItemId) {
+        this.setState({activeLink : linkItemId});
+    }
+
     render() {
+        /** Create link items */
+        let linkItems = [
+            {id : "dashboard_link_item", name : "Dashboard", active : false},
+            {id : "orders_link_item", name : "Orders", active : false},
+            {id : "products_link_item", name : "Products", active : false}
+        ];
+
+        const activeLink = this.state.activeLink;
+        linkItems.forEach(function(linkItem) {
+            linkItem.active = activeLink == linkItem.id;
+        });
+
+        /** Map link items */
+        let linkItemsView = linkItems.map((linkItem) =>
+            <SidebarLinkItem id={linkItem.id} active={linkItem.active} key={linkItem.id} name={linkItem.name} onItemClick={this.handleItemClick}/>
+        );
+
+        /** Rendering */
         return (
             <nav className="col-md-2 d-none d-md-block bg-light sidebar">
                 <div className="sidebar-sticky">
                     <ul className="nav flex-column">
-                        <li className="nav-item">
-                            <a className="nav-link active" href="#">
-                                <span data-feather="home"></span>
-                                Dashboard <span className="sr-only">(current)</span>
-                            </a>
-                        </li>
-
-                        <SidebarLinkItem name="Orders"/>
-                        <SidebarLinkItem name="Products"/>
-                        <SidebarLinkItem name="Customers"/>
-                        <SidebarLinkItem name="Reports"/>
-                        <SidebarLinkItem name="Integrations"/>
+                        {linkItemsView}
                     </ul>
                 </div>
             </nav>
@@ -27,14 +49,32 @@ class Sidebar extends React.Component{
     }
 }
 
+/**
+ * Sidebar link item's class
+ */
 class SidebarLinkItem extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.handleClick = this.handleClick.bind(this);
+    }
+
+    handleClick() {
+        this.props.onItemClick(this.props.id);
+    }
 
     render() {
         return(
             <li className="nav-item">
-                <a className="nav-link" href="#">
-                    {this.props.name}
-                </a>
+                {this.props.active == true ? (
+                    <a className="nav-link active" href="#" onClick={this.handleClick}>
+                        {this.props.name}
+                    </a>
+                ): (
+                    <a className="nav-link" href="#" onClick={this.handleClick}>
+                        {this.props.name}
+                    </a>
+                )}
             </li>
         )
     }
